806d44c37e182f691fc56aa97a142ec5456921e7
q2529608758 <q2529608758@163.com>
2025-07-06 23:36:35 +0800

    test

--- core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/BedrockSetLocalPlayerAsInitializedTranslator.java
+++ core/src/main/java/org/geysermc/geyser/translator/protocol/bedrock/BedrockSetLocalPlayerAsInitializedTranslator.java
@@ -64,6 +64,7 @@ public class BedrockSetLocalPlayerAsInitializedTranslator extends PacketTranslat
                 }
 
                 if (session.remoteServer().authType() == AuthType.OFFLINE){
+                    System.out.println(session.getPlayerEntity().getUsername());
                     String JavaUsername = CheckAccount(session.getPlayerEntity().getUsername());
                     if (JavaUsername == null){
                         session.disconnect("Hikari Network\n你没有注册账号或更改名称！");
